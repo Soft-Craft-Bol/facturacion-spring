804b6fe00c8566fa11385ff98c87906cbe843781
alfredoT7 <123456alfredotorri@gmail.com>
2025-02-07 08:30:42 -0400

    controladores items

--- src/main/java/com/gaspar/facturador/domain/repository/IItemRepository.java
+++ src/main/java/com/gaspar/facturador/domain/repository/IItemRepository.java
@@ -2,6 +2,7 @@ package com.gaspar.facturador.domain.repository;
 
 import com.gaspar.facturador.persistence.entity.ItemEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -9,4 +10,7 @@ import java.util.Optional;
 public interface IItemRepository {
 
     Optional<ItemEntity> findById(Integer id);
+    List<ItemEntity> findAll();
+    ItemEntity save(ItemEntity item);
+    void deleteById(Integer id);
 }
--- src/main/java/com/gaspar/facturador/persistence/entity/ItemEntity.java
+++ src/main/java/com/gaspar/facturador/persistence/entity/ItemEntity.java
@@ -27,4 +27,52 @@ public class ItemEntity {
     private BigDecimal precioUnitario;
 
     private Integer codigoProductoSin;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getUnidadMedida() {
+        return unidadMedida;
+    }
+
+    public void setUnidadMedida(Integer unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Integer getCodigoProductoSin() {
+        return codigoProductoSin;
+    }
+
+    public void setCodigoProductoSin(Integer codigoProductoSin) {
+        this.codigoProductoSin = codigoProductoSin;
+    }
 }
