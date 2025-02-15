000183359b67a33e94d363bb687a6438ebc161e7
alfredoT7 <123456alfredotorri@gmail.com>
2025-02-14 23:58:06 -0400

    facturas Horario controllers

---
+++ src/main/java/com/gaspar/facturador/persistence/entity/HorarioEntity.java
@@ -0,0 +1,35 @@
+package com.gaspar.facturador.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+@Entity
+@Table(name = "horarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HorarioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String panadero; // Nombre del panadero
+
+    private LocalTime horaEntrada;
+    private LocalTime horaSalida;
+
+    private LocalDate fechaEntrada;
+    private LocalDate fechaSalida; // Opcional
+
+    @ElementCollection
+    @CollectionTable(name = "dias_horario", joinColumns = @JoinColumn(name = "horario_id"))
+    @Column(name = "dia")
+    private List<String> dias;
+}
